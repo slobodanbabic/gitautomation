3880a5cffb156e944d7168094fe2e6caf30230d0
slobodanbabic <slobodannbabic@gmail.com>
2020-10-18 00:53:16 +0200

    edit Sample.java class

--- src/main/java/git/Sample.java
+++ src/main/java/git/Sample.java
@@ -1,39 +1,5 @@
 package git;
 
 public class Sample {
-  public void print416(int i) {
-    System.out.println(i);
-  }
-
-  public void print431(int i) {
-    System.out.println(i);
-  }
-
-  public void print2(int i) {
-    System.out.println(i);
-  }
-
-  public void print997(int i) {
-    System.out.println(i);
-  }
-
-  public void print479(int i) {
-    System.out.println(i);
-  }
-
-  public void print466(int i) {
-    System.out.println(i);
-  }
-
-  public void print967(int i) {
-    System.out.println(i);
-  }
-
-  public void print755(int i) {
-    System.out.println(i);
-  }
-
-  public void print23(int i) {
-    System.out.println(i);
-  }
+  
 }
